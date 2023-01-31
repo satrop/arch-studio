b746eff2f46d4fd22a4f6dbc8adc4beaecee9235
satrop <stevenportas@me.com>
2023-01-31 12:02:44 -0800

    Mobile nav done

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,114 @@
+import { Link, NavLink } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+
+import logo from '/img/logo.svg';
+
+const Nav = () => {
+	// const handleClick = () => {
+	// 	setActive((prevActive) => !prevActive);
+	// 	setIsPressed(!isPressed);
+	// };
+
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [isPressed, setIsPressed] = useState(false);
+	const [isActive, setIsActive] = useState(false);
+
+	const close = () => {
+		setMenuOpen(false);
+		setIsPressed(false);
+		setIsActive(false);
+	};
+	const open = () => {
+		setMenuOpen(true);
+		setIsPressed(true);
+		setIsActive(true);
+	};
+
+	const dropIn = {
+		hidden: {
+			x: '100%',
+		},
+		visible: {
+			x: '11%',
+			opacity: 1,
+		},
+		exit: {
+			y: '-100%',
+			opacity: 0,
+		},
+	};
+
+	return (
+		<nav className="main-nav">
+			<div className="mobile-nav-buttons">
+				<div className="content flex space-between align-center">
+					<Link to="/" aria-label="Home">
+						<img src={logo} alt="Logo" aria-label="Main logo" />
+					</Link>
+					<button
+						onClick={() => (menuOpen ? close() : open())}
+						className={`menu-button ${isActive ? 'active' : ''}`}
+						aria-pressed={isPressed}>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+				</div>
+			</div>
+			<div
+				className={`fade-out ${isActive ? 'active' : ''}`}
+				onClick={close}></div>
+			<AnimatePresence initial={false} onExitComplete={() => null}>
+				{menuOpen && (
+					<motion.ul
+						className="mobile-menu"
+						variants={dropIn}
+						initial="hidden"
+						animate="visible"
+						exit="exit">
+						<li>
+							<Link to="/" aria-label="Home">
+								<img
+									src={logo}
+									alt="Logo"
+									aria-label="Main logo"
+								/>
+							</Link>
+						</li>
+						<li>
+							<NavLink to="/about">About</NavLink>
+						</li>
+						<li>
+							<NavLink to="/contact">Contact</NavLink>
+						</li>
+						<li>
+							<NavLink to="/portfolio">Portfolio</NavLink>
+						</li>
+					</motion.ul>
+				)}
+			</AnimatePresence>
+			<div className="content">
+				<ul className="desktop-menu flex align-center">
+					<li>
+						<Link to="/" aria-label="Home">
+							<img src={logo} alt="Logo" aria-label="Main logo" />
+						</Link>
+					</li>
+					<li>
+						<NavLink to="/about">About</NavLink>
+					</li>
+					<li>
+						<NavLink to="/contact">Contact</NavLink>
+					</li>
+					<li>
+						<NavLink to="/portfolio">Portfolio</NavLink>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		devSourcemap: true,
+	},
 	base: '/arch-studio/',
 });
