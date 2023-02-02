3e2fa93e053ea59c37dd4621e831c16e59d9896b
satrop <stevenportas@me.com>
2023-02-01 16:01:47 -0800

    Image fixing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Button from '../Components/Button';
-import hero from '/home/mobile/image-hero-paramour.jpg';
+import hero from '/img/home/mobile/image-hero-paramour.jpg';
+
 import Swiper from '../Components/Swiper';
 
 import 'swiper/css';
